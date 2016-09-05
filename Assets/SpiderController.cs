ca79fc44ccf2ddf3a9bd093a8471a57d2fd2c993
hhassan1230 <hhassan@oswego.edu>
2016-09-04 20:59:15 -0400

    first commit and moving spider

---
+++ Assets/SpiderController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpiderController : MonoBehaviour {
+	private SpiderMovement spiderScript;
+	private GameObject spider;
+	public List<GameObject>  positionArray;
+	private int count; 
+	private int arrayLength;
+	private Vector3 currentPosition;
+	private float angle;
+
+	// Use this for initialization
+	void Start () {
+		spider = GameObject.FindGameObjectWithTag ("SpiderContainer");
+		spiderScript = spider.GetComponent<SpiderMovement> ();
+		arrayLength = positionArray.Count;
+		count = 0;
+		InvokeRepeating("UpdatePosition", 2f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void UpdatePosition(){
+		if (arrayLength > count) {
+			currentPosition = positionArray [count].transform.position;
+			spider.transform.rotation = Quaternion.LookRotation(spider.transform.position - positionArray [count].transform.position);
+
+			spiderScript.UpdateNextPosition (currentPosition);
+			count++;	
+		} else {
+			count = 0;
+			currentPosition = positionArray [count].transform.position;
+			spider.transform.rotation = Quaternion.LookRotation(spider.transform.position - positionArray [count].transform.position);
+
+			spiderScript.UpdateNextPosition (currentPosition);
+			count++;
+		}
+
+	}
+}
---
+++ Assets/SpiderMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderMovement : MonoBehaviour {
+	public static SpiderMovement instance;
+
+	private Vector3 currentPosition;
+	private Vector3 nextPosition;
+	private float moveU;
+	private float moveElapsed;
+	private bool isSpiderMoving;
+
+	public float duration = 1.5f;
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		isSpiderMoving = false;
+	}
+	
+	// Update is called once per frame
+
+	public void UpdateNextPosition(Vector3 position){
+		nextPosition = position;
+		isSpiderMoving = true;
+	}
+
+	void Update () {
+		if (isSpiderMoving) {
+			moveElapsed += Time.deltaTime;
+			moveU = moveElapsed / duration;
+			gameObject.transform.position = Vector3.Lerp(currentPosition, nextPosition, moveU);
+			if (moveElapsed >= duration){
+				isSpiderMoving = false;
+				moveElapsed = 0;
+				currentPosition = transform.position;
+			}  
+		}
+	}
+}
